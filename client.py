747b37cb44accf5b65793e9fc394ac7ad0411ca6
Ahmet Güler <glrahmtt@gmail.com>
2018-11-21 14:00:56 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
-#client.py
 import socket
 
-#create a socket object
+
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-#get local machine name
+
 host=socket.gethostname()
 
 port=9999
@@ -15,4 +14,4 @@ tm=s.recv(1024)
 
 s.close()
 
-print("The time got from the server is %s" % tm.decode('ascii'))
+print("Server'a Bağlantı Kuruldu :  %s" % tm.decode('ascii'))
